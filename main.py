b7cabdc5cc5238685a2d19792d6e340e14df3a6a
Krystian <28630895+Gryfin123@users.noreply.github.com>
2025-04-25 22:52:38 +0200

    Initial Version
    First working version. Fulfills most basic requirement.

---
+++ main.py
@@ -0,0 +1,139 @@
+'''
+To do:
+- Blacklistowanie kanałów
+- podział na serwery
+- wykluczanie botów (np. Avrae, użytkownicy tupperowi, etc.)
+- Liczenie Streaków na bierząco
+- kasowanie zapisków dłuższych niż tydzień (Streaków których ostatnia wiadomość jest starsza niż tydzień)
+- 
+
+
+'''
+import discord
+import datetime
+
+class TrackerGlobal:
+    streakLength = 30 # in minutes
+    def __init__(self):
+        self.trackers = []
+
+    def findUser(self, userId):
+        for x in self.trackers:
+            if x.userId == userId:
+                return x
+        return False
+
+    def NoteMessage(self, message):
+        result = self.findUser(message.author.id)
+        if result == False:
+            # Add new user tracker
+            print(f"Registring first message of {message.author}")
+            newTracker = TrackerUser(message.author.id)
+            self.trackers.append(newTracker)
+            newTracker.AddMessage(message)
+        else:
+            # Add message to existing tracker
+            result.AddMessage(message)
+
+    def RequestRaport(self, user):
+        result = self.findUser(user.id)
+        if result == False:
+            return f"There are no noted messages sent by {user.global_name}"
+        else:
+            return result.GetRaport(self.streakLength)
+
+class TrackerServer:
+    pass
+
+
+
+
+class TrackerUser:
+    def __init__(self, userId):
+        self.userId = userId
+        self.messageList = {}
+
+    def AddMessage(self, message):
+        self.messageList[message.created_at] = message.jump_url
+        print(f"Noted message with link ({message.jump_url}) and timestamp ({message.created_at})")
+
+    def GetRaport(self, streakLength):
+        finalString = ""
+
+        streakBeg = False
+        streakLast = False
+
+        for msgTime in self.messageList:
+            # If it's first list element
+            if streakBeg == False:
+                streakBeg = msgTime   
+                streakLast = msgTime
+                continue
+
+            # Check if time between last msg and current is lesser then streak length
+            if streakLast + datetime.timedelta(minutes=streakLength) > msgTime:
+                # If so, update streakLast and go to next message
+                streakLast = msgTime
+                continue
+            else:
+                # If not, check how much time passed between streakBeg and streakLast, 
+                #  prepare and append final string 
+                #  and set streakBeg to this message.
+                dif = streakLast - streakBeg
+                link1 = self.messageList[streakBeg]
+                link2 = self.messageList[streakLast]
+                finalString += f"Streak that from {streakBeg.strftime("%d/%m/%Y")} lasted {(dif.seconds // 3600):02d}:{((dif.seconds % 3600) // 60):02d}:{(dif.seconds % 60):02d} from {link1} to {link2}\n"
+
+                streakBeg = msgTime
+                streakLast = msgTime
+
+        # when all messages have been checked, close the last -
+        dif = streakLast - streakBeg
+        link1 = self.messageList[streakBeg]
+        link2 = self.messageList[streakLast]
+        finalString += f"Streak that from {streakBeg.strftime("%d/%m/%Y")} lasted {(dif.seconds // 3600):02d}:{((dif.seconds % 3600) // 60):02d}:{(dif.seconds % 60):02d} from {link1} to {link2}\n"
+
+        return finalString
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Process
+print("I start!")
+
+class MyClient(discord.Client):
+    globalTracker = TrackerGlobal()
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        elif message.content == "Angelotron raport":
+            text = self.globalTracker.RequestRaport(message.author)
+            await message.channel.send(text)
+
+        else:
+            # Note message in user list.
+            print(f"{message.author} has sent a message.")
+            self.globalTracker.NoteMessage(message)
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run('[token]')
